04b97f82f34f89ba5825b0455dde0b087d04eb85
vagrant <vagrant@test-ub>
2023-08-14 07:23:00 +0000

    new version

--- count_lines.py
+++ count_lines.py
@@ -1,7 +1,8 @@
 import os
 
 total_lines = 0
-
+print('Добро пожаловать в count_lines')
+print('Запускаю подсчет количества строк в директории где расположен скрипт')
 for filename in os.listdir('.'):
     if filename.endswith('.txt'):
         with open(filename, 'r') as file:
